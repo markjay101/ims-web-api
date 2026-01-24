645601f5c5e1cba65caede5700eb6b1ea52b887a
markjay <markjayfuentescuyos1@gmail.com>
2026-01-24 20:02:26 +0800

    add modem api endpoints

---
+++ IMS.WebApi/Controllers/ModemsController.cs
@@ -0,0 +1,44 @@
+﻿using IMS.Application.Common.Models;
+using IMS.Application.Features.Modems.Commands.CreateModem;
+using IMS.Application.Features.Modems.Queries;
+using IMS.WebApi.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IMS.WebApi.Controllers
+{
+    public class ModemsController : ApiControllerBase
+    {
+        [HttpGet]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ApiResponse<PaginatedList<ModemDto>>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse<PaginatedList<ModemDto>>), StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetModems([FromQuery] GetModemsQuery query)
+        {
+            var result = await Mediator.Send(query);
+
+            if (result.Items.Count > 0)
+                return Ok(ApiResponse<PaginatedList<ModemDto>>.Success(result));
+
+            return StatusCode(StatusCodes.Status204NoContent, ApiResponse<PaginatedList<ModemDto>>.Success(result, "No modems found."));
+        }
+
+        [HttpPost]
+        [Produces("application/json")]
+        [ProducesResponseType(typeof(ApiResponse<Guid>), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateModem([FromBody] CreateModemCommand command)
+        {
+            var result = await Mediator.Send(command);
+
+            if (result != Guid.Empty)
+                return StatusCode(201, ApiResponse<Guid>.Success(result, "Modem successfully created."));
+
+            return BadRequest(ApiResponse<object>.Failure(["Empty Guid"], "Failed to create modem."));
+        }
+    }
+}
