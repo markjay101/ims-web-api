9905fe527afc69782e3889d5ca29f5df397e49e0
markjay <markjayfuentescuyos1@gmail.com>
2026-02-03 17:52:31 +0800

    add update modem command and validator

---
+++ IMS.Application/Features/Modems/Commands/UpdateModem/UpdateModemCommand.cs
@@ -0,0 +1,23 @@
+﻿using IMS.Application.Common.Interfaces;
+using MediatR;
+
+namespace IMS.Application.Features.Modems.Commands.UpdateModem
+{
+    public record UpdateModemCommand(Guid Id, string Model, string SerialNumber, string MacAddress) : IRequest<bool>;
+    internal class UpdateModemCommandHandler(IApplicationDbContext context) : IRequestHandler<UpdateModemCommand, bool>
+    {
+        public async Task<bool> Handle(UpdateModemCommand request, CancellationToken cancellationToken)
+        {
+            var modem = await context.Modems.FindAsync(request.Id, cancellationToken);
+          
+            modem?.Model = request.Model;
+            modem?.SerialNumber = request.SerialNumber;
+            modem?.MacAddress = request.MacAddress;
+
+            var result = await context.SaveChangesAsync(cancellationToken);
+
+            return result > 0;
+        }
+    {
+    }
+}
---
+++ IMS.Application/Features/Modems/Commands/UpdateModem/UpdateModemCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using IMS.Application.Common.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Application.Features.Modems.Commands.UpdateModem
+{
+    public class UpdateModemCommandValidator : AbstractValidator<UpdateModemCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateModemCommandValidator(IApplicationDbContext context)
+        {
+            _context = context;
+
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("The Id field is required.")
+                .MustAsync(ModemShouldExist).WithMessage("Modem with the specified Id does not exist.");
+
+            RuleFor(x => x.Model)
+                .NotEmpty().WithMessage("The Model field is required.");
+
+            RuleFor(x => x.SerialNumber)
+                .NotEmpty().WithMessage("The SerialNumber field is required.");
+
+            RuleFor(x => x.MacAddress)
+                .NotEmpty().WithMessage("The MacAddress field is required.");
+        }
+
+        private async Task<bool> ModemShouldExist(Guid guid, CancellationToken token)
+        {
+            return await _context.Modems.AnyAsync(m => m.Id == guid, token);
+        }
+    }
+}
