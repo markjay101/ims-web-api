e0fbfb6a553637121756ecbcb5efd199d226312d
markjay <markjayfuentescuyos1@gmail.com>
2026-02-10 14:43:46 +0800

    migration: add customer plan and internet plan relationship

---
+++ IMS.Infrastructure/Persistence/Migrations/20260210063825_AddCustomerPlanAndInternetPlanRelationship.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IMS.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerPlanAndInternetPlanRelationship : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerPlans_InternetPlanId",
+                table: "CustomerPlans",
+                column: "InternetPlanId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CustomerPlans_InternetPlans_InternetPlanId",
+                table: "CustomerPlans",
+                column: "InternetPlanId",
+                principalTable: "InternetPlans",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CustomerPlans_InternetPlans_InternetPlanId",
+                table: "CustomerPlans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CustomerPlans_InternetPlanId",
+                table: "CustomerPlans");
+        }
+    }
+}
