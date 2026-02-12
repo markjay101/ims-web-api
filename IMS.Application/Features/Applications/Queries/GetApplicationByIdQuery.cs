a2e43d1381ea4be60558d0d9e3460f7cfa89e23e
markjay <markjayfuentescuyos1@gmail.com>
2026-02-12 12:37:51 +0800

    add GetApplicationByIdQuery

---
+++ IMS.Application/Features/Applications/Queries/GetApplicationByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using IMS.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace IMS.Application.Features.Applications.Queries
+{
+    public record GetApplicationByIdQuery(Guid Id) : IRequest<ApplicationDto?>;
+    public class GetApplicationByIdQueryHandler(IApplicationDbContext context, IMapper mapper) : IRequestHandler<GetApplicationByIdQuery, ApplicationDto?>
+    {
+        public async Task<ApplicationDto?> Handle(GetApplicationByIdQuery request, CancellationToken cancellationToken)
+        {
+            var application = await context.Applications
+                                            .AsNoTracking()
+                                            .FirstOrDefaultAsync(a => a.Id == request.Id, cancellationToken);
+
+            
+            return mapper.Map<ApplicationDto>(application);
+        }
+    }
+}
