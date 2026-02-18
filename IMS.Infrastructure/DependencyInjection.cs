c57ff3ca5519eb02485cb18bd72e80c1674e761e
markjay <markjayfuentescuyos1@gmail.com>
2026-02-18 14:58:41 +0800

    refactor: remove unuse namespaces and remove comment in the email template

--- IMS.Infrastructure/DependencyInjection.cs
+++ IMS.Infrastructure/DependencyInjection.cs
@@ -10,9 +10,6 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
 namespace IMS.Infrastructure
 {
