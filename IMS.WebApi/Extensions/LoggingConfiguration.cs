76a0cd3528254bb6d842bd1ee403b5901fba00af
markjay <markjayfuentescuyos1@gmail.com>
2026-02-24 16:13:46 +0800

    feat(logging): implement serilog

---
+++ IMS.WebApi/Extensions/LoggingConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Serilog;
+using Serilog.Events;
+
+namespace IMS.WebApi.Extensions
+{
+    public static class LoggingConfiguration
+    {
+        public static void UseSerilog(this WebApplicationBuilder builder)
+        {
+            var logTemplate = builder.Configuration["LogTemplate"]!;
+
+            Log.Logger = new LoggerConfiguration()
+                                .ReadFrom.Configuration(builder.Configuration)
+                                .WriteTo.Logger(lc => lc
+                                    .Filter.ByExcluding(logEvent =>
+                                        logEvent.Level < LogEventLevel.Warning
+                                        && logEvent.Properties.ContainsKey("SourceContext")
+                                        && logEvent.Properties["SourceContext"].ToString().Contains("Microsoft.EntityFrameworkCore")
+                                        || logEvent.MessageTemplate.Text.Contains("Application started. Press Ctrl+C to shut down"))
+                                    .WriteTo.Console(outputTemplate: logTemplate)
+                                    .WriteTo.File(builder.Configuration["LogPath"]!, 
+                                                    rollingInterval: RollingInterval.Day, 
+                                                    outputTemplate: logTemplate))
+                                .CreateLogger();
+
+            builder.Host.UseSerilog();
+        }
+    }
+}
--- IMS.WebApi/Program.cs
+++ IMS.WebApi/Program.cs
@@ -2,16 +2,35 @@ using IMS.Application;
 using IMS.Infrastructure;
 using IMS.Infrastructure.Persistence;
 using IMS.WebApi;
+using IMS.WebApi.Extensions;
 using IMS.WebApi.Middlewares;
 using Scalar.AspNetCore;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.UseSerilog();
 
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddApplicationServices();
 builder.Services.AddWebApiServices(builder.Configuration);
 
+
 var app = builder.Build();
+
+app.UseSerilogRequestLogging(options =>
+{
+    options.MessageTemplate = builder.Configuration["LogRequestTemplate"]!;
+
+    // Filter out noisy requests like Swagger/Scalar UI or Health Checks
+    options.GetLevel = (httpContext, elapsed, ex) =>
+        (ex != null || httpContext.Response.StatusCode > 499)
+            ? LogEventLevel.Error
+            : (httpContext.Request.Path.StartsWithSegments("/scalar") || httpContext.Request.Path.StartsWithSegments("/openapi"))
+                ? LogEventLevel.Verbose
+                : LogEventLevel.Information;
+});
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
