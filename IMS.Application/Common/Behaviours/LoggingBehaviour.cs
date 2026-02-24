6781cc069efc9a1edf37cdc56898f17cf80b00ce
markjay <markjayfuentescuyos1@gmail.com>
2026-02-24 16:36:47 +0800

    feat(logging): add logging behaviour

---
+++ IMS.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using IMS.Application.Common.Interfaces;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace IMS.Application.Common.Behaviours
+{
+    public class LoggingBehaviour<TRequest, TRespnse>(ILogger<LoggingBehaviour<TRequest, TRespnse>> logger, ICurrentUserService currentUserService) : IPipelineBehavior<TRequest, TRespnse> where TRequest: notnull
+    {
+        public async Task<TRespnse> Handle(TRequest request, RequestHandlerDelegate<TRespnse> next, CancellationToken cancellationToken)
+        {
+            var userId = currentUserService?.UserId ?? string.Empty;
+            var userName = currentUserService?.Email ?? string.Empty;
+
+            logger.LogInformation("Handling {RequestName} - User: {Username} [{UserId}]", typeof(TRequest).Name, userName, userId);
+
+            var timer = Stopwatch.StartNew();
+
+            var response = await next();
+
+            timer.Stop();
+
+            logger.LogInformation("Finished {RequestName} in {Elapsed}ms",
+                typeof(TRequest).Name, timer.ElapsedMilliseconds);
+
+            return response;
+        }
+    }
+}
--- IMS.Application/DependencyInjection.cs
+++ IMS.Application/DependencyInjection.cs
@@ -12,6 +12,7 @@ namespace IMS.Application
         {
             services.AddMediatR(cfg => {
                 cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehaviour<,>));
                 cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
                 cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
             });
