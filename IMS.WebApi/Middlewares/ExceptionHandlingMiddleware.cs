d3a82f47d43efc4c651b2dd0a755171e2677d7fa
markjay <markjayfuentescuyos1@gmail.com>
2026-02-24 16:48:08 +0800

    feat(logging): add logging in the exceptionhandlingmiddleware

--- IMS.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
+++ IMS.WebApi/Middlewares/ExceptionHandlingMiddleware.cs
@@ -3,34 +3,36 @@ using IMS.WebApi.Common;
 
 namespace IMS.WebApi.Middlewares
 {
-    public class ExceptionHandlingMiddleware
+    public class ExceptionHandlingMiddleware(ILogger<ExceptionHandlingMiddleware> logger, RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
-
-        public ExceptionHandlingMiddleware(RequestDelegate next) => _next = next;
-
         public async Task InvokeAsync(HttpContext context)
         {
             try
             {
-                await _next(context);
+                await next(context);
             }
             catch (ValidationException ex) //fluent validation exception
             {
+                logger.LogError(ex, "Validation error occurred");
+
                 context.Response.StatusCode = StatusCodes.Status400BadRequest;
                 var errors = ex.Errors.Select(e => e.ErrorMessage).ToList();
 
-                var response = ApiResponse<object>.Failure(errors, "Validation failed.");
+                var response = ApiResponse<object>.Failure(errors, "Validation failed");
 
                 await context.Response.WriteAsJsonAsync(response);
             }
             catch (UnauthorizedAccessException uaex)
             {
+                logger.LogError(uaex, "Unauthorized access error occurred");
+
                 context.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                await context.Response.WriteAsJsonAsync(ApiResponse<object>.Failure([uaex.Message], "Unauthorized."));
+                await context.Response.WriteAsJsonAsync(ApiResponse<object>.Failure([uaex.Message], "Unauthorized"));
             }
             catch (Exception ex)
             {
+                logger.LogError(ex, "An unexpected error occurred");
+
                 context.Response.StatusCode = StatusCodes.Status500InternalServerError;
                 await context.Response.WriteAsJsonAsync(ApiResponse<object>.Failure([ex.Message], "Server Error"));
             }
